8910af68dea86e520b1ec167813f9c73b8a66ca8
Oyunmyagmar <oyunmyagmar.g@gmail.com>
2026-02-06 08:43:41 +0800

    added test function on club-dash

--- apps/club-dash/app/page.tsx
+++ apps/club-dash/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
 export default function TestComponent() {
-  return <div onClick={() => console.log('hi')}>Hello</div>;
+  return (
+    <div
+      onClick={() => console.log('hi')}
+      className="cursor-pointer bg-red-500 text-white"
+    >
+      Hello
+    </div>
+  );
 }
