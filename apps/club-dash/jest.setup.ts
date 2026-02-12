27762b3caaf2cc9b8ec024c526ba51df6e7a2917
25LP8609 <25lp8609@Mac-20s-Mac-mini.local>
2026-02-12 14:46:26 +0800

    fix 2.12

--- apps/club-dash/jest.setup.ts
+++ apps/club-dash/jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+class PointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+  constructor(type: string, props: PointerEventInit = {}) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || 'mouse';
+  }
+}
+window.PointerEvent = PointerEvent as any;
+
+Element.prototype.scrollIntoView = jest.fn();
