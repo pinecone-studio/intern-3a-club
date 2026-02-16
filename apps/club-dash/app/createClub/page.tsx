1a8ffa96fafdcfceb83d2d6479ecdd81379a28ca
Khishigdari <nbhishgee22@gmail.com>
2026-02-16 12:55:35 +0800

    calendar 2.16

--- apps/club-dash/app/createClub/page.tsx
+++ apps/club-dash/app/createClub/page.tsx
@@ -98,7 +98,9 @@ const CreateClub = () => {
                 onChange={handleClubDesc}
               />
             </div>
-            <div className="space-y-6 md:grid md:grid-cols-2">
+            {/* <div className="space-y-6 gap-6 md:grid md:grid-cols-2"> */}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="flex flex-col gap-2">
                 <Label
                   htmlFor="timetable"
