a3ad3b8a9757ac65afefca7e82062fe716c2a708
oyunmyagmar <25LP4018@nest.edu.mn>
2026-02-24 21:08:25 +0800

    club timetable edit

--- apps/club-dash/app/testForBackend/page.tsx
+++ apps/club-dash/app/testForBackend/page.tsx
@@ -53,6 +53,7 @@ export default function CreateClubPage() {
                   <div>{schedule.room}</div>
                   <div>{schedule.clubStartTime}</div>
                   <div>{schedule.duration}</div>
+                  <Button>Edit</Button>
                 </div>
               ))}
             </div>
