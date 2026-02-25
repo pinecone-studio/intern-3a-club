a7c51a23f0539e128fe9146e62817919e4914362
25LP9779 <25lp9779@admins-Mac-mini.local>
2026-02-25 18:53:30 +0800

    page.tsx kocd

--- apps/club-web/app/JoinClub/page.tsx
+++ apps/club-web/app/JoinClub/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useQuery } from '@apollo/client/react';
-import { ClubsContent } from './_components/ClubsContent';
+// import { ClubsContent } from './_components/ClubsContent';
 import { Data, GET_ALL_CLUBS } from '../../lib/type';
 
 const JoinClubPage = () => {
@@ -11,7 +11,7 @@ const JoinClubPage = () => {
 
   return (
     <div className="relative min-h-screen w-full bg-[#050a12] bg-[radial-gradient(circle_at_20%_30%,_rgba(29,78,216,0.25)_0%,_transparent_30%),_radial-gradient(circle_at_30%_10%,_rgba(100,116,139,0.15)_0%,_transparent_60%)] overflow-hidden">
-      <ClubsContent />
+      {/* <ClubsContent /> */}
     </div>
   );
 };
