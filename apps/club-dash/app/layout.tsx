d05df437a47e3618a07e0a0663e53b7cbec2b7c3
Ochko1105 <tsetsegulziiocherdene@gmail.com>
2026-02-28 14:46:27 +0800

    176 branch change for office (#199)
    * deployments and scripts done
    
    * ignore .vercel lint test
    
    * pre push test
    
    * fix test
    
    ---------
    
    Co-authored-by: Ochko <ochko@G303-CC0140.local>

--- apps/club-dash/app/layout.tsx
+++ apps/club-dash/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Providers } from '../libs/apollo/providers';
+import { Providers } from '../libs/apollo/Providers';
 import './global.css';
 import { ClerkProvider } from '@clerk/nextjs';
 
