cb26932a997a58ff78b8f9b26b2457ae8e6e07cd
hatakiii <25lp5598@nest.edu.mn>
2026-03-05 00:07:26 +0800

    teacher section and time interval fixed

--- apps/club-web/components/create-club/TeacherSelect.tsx
+++ apps/club-web/components/create-club/TeacherSelect.tsx
@@ -22,6 +22,8 @@ export const TeacherSelect = ({
   const teacherList =
     teachers && teachers.length > 0 ? teachers : FALLBACK_TEACHERS;
 
+  console.log('teacherList', teacherList);
+
   return (
     <div className="relative">
       <select
@@ -32,11 +34,7 @@ export const TeacherSelect = ({
         className={className}
       >
         <option value="">Сонгох...</option>
-        {teacherList.map((t) => (
-          <option key={t.id} value={t.id}>
-            {t.lastName ? `${t.firstName} ${t.lastName}` : t.firstName}
-          </option>
-        ))}
+        <option value="teacher">Багш</option>
         <option value="student">Сурагч</option>
       </select>
       <MoveDownIcon className="absolute right-4 top-1/2 -translate-y-1/2 h-4 w-4 text-white/20" />
